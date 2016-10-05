8bef9788468b1df121ce3351a2690eb685c96f68
Leif Battermann <leifbattermann@gmail.com>
2016-10-05 17:32:39 +0200

    solutions for problems 1 and 2

--- src/main/scala/Problem01.scala
+++ src/main/scala/Problem01.scala
@@ -1,5 +1,5 @@
 package NinetyNineProblems
 
 object P01 {
-  def last[A](xs:List[A]) : A = ???
+  def last[A](xs:List[A]) : A = xs.last
 }
--- src/main/scala/Problem02.scala
+++ src/main/scala/Problem02.scala
@@ -1,5 +1,11 @@
 package NinetyNineProblems
 
+import java.util.NoSuchElementException
+
 object P02 {
-  def penultimate[A](xs:List[A]) : A = ???
+  def penultimate[A](xs:List[A]) : A = xs match {
+    case x :: _ :: Nil => x
+    case h :: tail     => penultimate(tail)
+    case _             => throw new NoSuchElementException
+  }
 }
